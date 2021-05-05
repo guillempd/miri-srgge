9c025211b20feaee2ef33b45f31432e8941020b9
Guillem Perez Delgado <guillem.perez.delgado@gmail.com>
2021-05-05 18:14:08 +0200

    improved mesh simplifier removing collapsed faces and repeated faces

--- MeshSimplifier.cpp
+++ MeshSimplifier.cpp
@@ -3,9 +3,14 @@
 #include "PLYWriter.h"
 #include "TriangleMesh.h"
 
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/glm.hpp>
+#include <glm/gtx/hash.hpp> // Experimental feature to provide hashing capabilities
+
 #include <iostream>
 #include <string>
 #include <unordered_map>
+#include <unordered_set>
 
 // TODO: Take into account the multiple LODS
 TriangleMesh SimplifyMesh(const TriangleMesh &mesh)
@@ -13,7 +18,6 @@ TriangleMesh SimplifyMesh(const TriangleMesh &mesh)
     TriangleMesh simplifiedMesh;
     Octree octree(mesh.aabb);
     std::vector<OctreeNode*> representative(mesh.vertices.size(), nullptr);
-
     // Compute representatives
     for (int i = 0; i < mesh.vertices.size(); ++i)
     {
@@ -24,6 +28,7 @@ TriangleMesh SimplifyMesh(const TriangleMesh &mesh)
     // Add representatives to simplifiedMesh
     // If needed (QEM) compute them
     std::unordered_map<OctreeNode*, int> simplifiedMeshVertices;
+    std::unordered_map<int, int> originalToSimplifiedIndex;
     int j = 0;
     for (int i = 0; i < mesh.vertices.size(); ++i)
     {
@@ -31,22 +36,55 @@ TriangleMesh SimplifyMesh(const TriangleMesh &mesh)
         if (!vertex_found)
         {
             simplifiedMesh.addVertex(representative[i]->average);
-            simplifiedMeshVertices[representative[i]] = j++;
+            simplifiedMeshVertices[representative[i]] = j;
+            originalToSimplifiedIndex[i] = j++;
+        }
+        else
+        {
+            originalToSimplifiedIndex[i] = simplifiedMeshVertices[representative[i]];
         }
     }
 
     // Add faces to simplifiedMesh
-    // TODO: Removing duplicates
+    std::unordered_set<glm::ivec3> simplifiedMeshTriangles;
     for (int i = 0; i < mesh.triangles.size(); i += 3)
     {
         int i0 = mesh.triangles[i];
         int i1 = mesh.triangles[i + 1];
         int i2 = mesh.triangles[i + 2];
-        simplifiedMesh.addTriangle(
-            simplifiedMeshVertices[representative[i0]],
-            simplifiedMeshVertices[representative[i1]],
-            simplifiedMeshVertices[representative[i2]]
-            );
+
+        int j0 = originalToSimplifiedIndex[i0];
+        int j1 = originalToSimplifiedIndex[i1];
+        int j2 = originalToSimplifiedIndex[i2];
+
+        // If face collapsed to edge or point skip
+        if (j0 == j1) continue;
+        if (j1 == j2) continue;
+        if (j2 == j0) continue;
+
+        // Reorder indices so the first one is the smallest
+        if (j1 < j0 && j1 < j2)
+        {
+            int aux = j0;
+            j0 = j1;
+            j1 = j2;
+            j2 = aux;
+        }
+        else if (j2 < j0 && j2 < j1)
+        {
+            int aux = j0;
+            j0 = j2;
+            j2 = j1;
+            j1 = aux;
+        }
+
+        glm::ivec3 triangle(j0, j1, j2);
+        bool triangle_found = (simplifiedMeshTriangles.find(triangle) != simplifiedMeshTriangles.end());
+        if (!triangle_found)
+        {
+            simplifiedMesh.addTriangle(j0, j1, j2);
+            simplifiedMeshTriangles.insert(triangle);
+        }
     }
     return simplifiedMesh;
 }
